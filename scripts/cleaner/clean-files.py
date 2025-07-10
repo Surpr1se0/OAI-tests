353f6a6f745e264c0ad4b63cd85906837ea96fdd
Surpr1se0 <franciscoggouveia@gmail.com>
2025-07-10 18:07:39 +0100

    normalized time in clean-files.py and .log files

--- scripts/cleaner/clean-files.py
+++ scripts/cleaner/clean-files.py
@@ -1,24 +1,45 @@
 import re
 import os
 
+# General variables
 scenarios = ["cf", "cb"] # cell-free and cell-based
 reps = ["1", "5", "10"]  # can be 1, 5 or 10
 base_dir_input = os.path.join("dirty-files")
 base_dir_output = os.path.join("clean-files")
 
+# Function to extract and clean the CSV data
 def extract_csv(input, output):
+    # regex
     pattern = re.compile(r"(\d+)\[UE (\d+) \| RB (\d+)\] Throughput = ([\d.]+) Mbps")
 
+    # open the input file and read lines
     with open(input, 'r') as f:
         lines = f.readlines()
 
+    # process each line
+    parsed_lines = []
+    for line in lines:
+        match = pattern.search(line)
+        if match:
+            # extract the relevant data
+            time_ms, ue_id, rb_id, throughput = match.groups()
+            # convert to appropriate types
+            parsed_lines.append((time_ms, ue_id, rb_id, throughput))
+    
+    # check if we have valid data
+    if not parsed_lines:
+        print(f"No valid data found in {input}")
+        return
+
+    # calculate the minimum time
+    min_time = min(int(row[0]) for row in parsed_lines)
+
+    # create the ouput file and write the header
     with open(output, 'w') as f_out:
-        f_out.write("Time_ms,UE_ID,RB_ID,Throughput_Mbps\n")  # header
-        for linha in lines:
-            match = pattern.search(linha)
-            if match:
-                time_ms,ue_id, rb_id, throughput = match.groups()
-                f_out.write(f"{time_ms},{ue_id},{rb_id},{throughput}\n")
+        f_out.write("Time_ms,UE_ID,RB_ID,Throughput_Mbps\n")
+        for time_ms, ue_id, rb_id, throughput in parsed_lines:
+            norm_time = int(time_ms) - min_time # regularize the time to start from 0
+            f_out.write(f"{norm_time},{ue_id},{rb_id},{throughput}\n")
 
     print(f"Clean file created: {output}\n")
 
